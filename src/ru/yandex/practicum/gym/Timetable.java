4f40c2f87dd0862de45370971c612607f67d3d14
tentateursss <Mativ4ik77@yandex.ru>
2026-01-20 13:39:31 +0300

    Изменения в Timetable.java

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -11,54 +11,51 @@ public class Timetable {
         DayOfWeek dayOfWeek = trainingSession.getDayOfWeek();
         TimeOfDay timeOfDay = trainingSession.getTimeOfDay();
 
-        TreeMap<TimeOfDay, TrainingSession> dayTraining = timetable.get(dayOfWeek);
-        if (dayTraining == null) {
-            dayTraining = new TreeMap<>();
-            timetable.put(dayOfWeek, dayTraining);
+        TreeMap<TimeOfDay, TrainingSession> day = timetable.get(dayOfWeek);
+        if (day == null) {
+            day = new TreeMap<>();
+            timetable.put(dayOfWeek, day);
         }
 
-        dayTraining.put(timeOfDay, trainingSession);
+        day.put(timeOfDay, trainingSession);
     }
 
     public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
-        TreeMap<TimeOfDay, TrainingSession> dayTraining = timetable.get(dayOfWeek);
-
-        if (dayTraining == null || dayTraining.isEmpty()) {
+        TreeMap<TimeOfDay, TrainingSession> day = timetable.get(dayOfWeek);
+        if (day == null) {
             return Collections.emptyList();
         }
 
-        return new ArrayList<>(dayTraining.values());
+        return new ArrayList<>(day.values());
     }
 
     public TrainingSession getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
-        TreeMap<TimeOfDay, TrainingSession> dayTraining = timetable.get(dayOfWeek);
-
-        if (dayTraining == null) {
+        TreeMap<TimeOfDay, TrainingSession> day = timetable.get(dayOfWeek);
+        if (day == null) {
             return null;
         }
 
-        return dayTraining.get(timeOfDay);
+        return day.get(timeOfDay);
     }
 
     public Map<String, Integer> getCountByCoaches() {
-        Map<Coach, Integer> coachCounter = new HashMap<>();
-
+        Map<Coach, Integer> integerMap = new HashMap<>();
         for (TreeMap<TimeOfDay, TrainingSession> value : timetable.values()) {
             for (TrainingSession trainingSession : value.values()) {
                 Coach coach = trainingSession.getCoach();
-                coachCounter.put(coach, coachCounter.getOrDefault(coach, 0) + 1);
+                integerMap.put(coach, integerMap.getOrDefault(coach, 0) + 1);
             }
         }
 
-        List<CounterOfTrainings> counterOfTrainingsList = new ArrayList<>();
-        for (Map.Entry<Coach, Integer> entry : coachCounter.entrySet()) {
-            CounterOfTrainings counterOfTrainings = new CounterOfTrainings(entry.getKey(), entry.getValue());
-            counterOfTrainingsList.add(counterOfTrainings);
+        List<CounterOfTrainings> counterOfTrainings = new ArrayList<>();
+        for (Map.Entry<Coach, Integer> entry : integerMap.entrySet()) {
+            CounterOfTrainings counterOfTrainings1 = new CounterOfTrainings(entry.getKey(), entry.getValue());
+            counterOfTrainings.add(counterOfTrainings1);
         }
 
-        Collections.sort(counterOfTrainingsList, new Comparator<CounterOfTrainings>() {
+        Collections.sort(counterOfTrainings, new Comparator<CounterOfTrainings>() {
             @Override
             public int compare(CounterOfTrainings o1, CounterOfTrainings o2) {
                 return o2.getCount() - o1.getCount();
@@ -66,8 +63,8 @@ public class Timetable {
         });
 
         Map<String, Integer> result = new LinkedHashMap<>();
-        for (CounterOfTrainings counterOfTrainings : counterOfTrainingsList) {
-            result.put(counterOfTrainings.getCoach().getSurname(), counterOfTrainings.getCount());
+        for (CounterOfTrainings counterOfTraining : counterOfTrainings) {
+            result.put(counterOfTraining.getCoach().getSurname(), counterOfTraining.getCount());
         }
 
         return result;
